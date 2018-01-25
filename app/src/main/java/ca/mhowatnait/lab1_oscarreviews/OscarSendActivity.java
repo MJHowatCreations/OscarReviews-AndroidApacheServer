daaa5f5cc4a4205df6d2ad816ae72bbbb2b1fc9f
Matthew Howat <matthewj.howat@gmail.com>
2018-01-25 14:50:50 -0700

    working on post

--- app/src/main/java/ca/mhowatnait/lab1_oscarreviews/OscarSendActivity.java
+++ app/src/main/java/ca/mhowatnait/lab1_oscarreviews/OscarSendActivity.java
@@ -2,6 +2,7 @@ package ca.mhowatnait.lab1_oscarreviews;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -17,8 +20,35 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
-public class OscarSendActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class OscarSendActivity extends AppCompatActivity implements View.OnClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
     SharedPreferences settings;
+    View mainView;
+
+
+//    OVERRIDE METHODS
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oscar_send);
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mainView = findViewById(R.id.layout_send_activity);
+        String bgColor = settings.getString("main_bg_color_list", "#FFFFFF" );
+
+        mainView.setBackgroundColor(Color.parseColor(bgColor));
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        String bgColor = settings.getString("main_bg_color_list", "#FFFFFF" );
+        mainView.setBackgroundColor(Color.parseColor(bgColor));
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,14 +81,40 @@ public class OscarSendActivity extends AppCompatActivity implements View.OnClick
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_oscar_send);
-        settings = PreferenceManager.getDefaultSharedPreferences(this);
-    }
-
-    @Override
     public void onClick(View view) {
 
+
     }
+
+//    METHODS
+
+    private void postToChatter()
+    {
+        EditText editTextReview = (EditText)findViewById(R.id.edit_text_review);
+        EditText editTextNominee = (EditText)findViewById(R.id.edit_text_nominee);
+        String chatReview = editTextReview.getText().toString();
+        String chatNominee = editTextNominee.getText().toString();
+        String userName = settings.getString("reviewer_name", "Harvey Weinstein");
+        String password = settings.getString("user_password", "none");
+        try
+        {
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost("http://www.youcode.ca/Lab01Servlet");
+            List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+            postParameters.add(new BasicNameValuePair("DATA", chat));
+            postParameters.add(new BasicNameValuePair("LOGIN_NAME", userName));
+            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(postParameters);
+            post.setEntity(formEntity);
+            client.execute(post);
+
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(this, "Error:" + e, Toast.LENGTH_LONG).show();
+        }
+        edittext.getText().clear();
+    }
+
+
+
 }
